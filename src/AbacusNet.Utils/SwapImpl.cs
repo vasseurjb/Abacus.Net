c5f4f0a851930e6fd01375071120ba350e0b61f6
vasseurjb <vasseurjb@gmail.com>
2022-04-06 19:39:26 +0200

    add namespace

--- src/AbacusNet.Utils/SwapImpl.cs
+++ src/AbacusNet.Utils/SwapImpl.cs
@@ -1,33 +1,34 @@
 ﻿using System.Numerics;
 
-namespace AbacusNet.Utils;
-
-public static class SwapImpl
+namespace AbacusNet.Utils
 {
-    public static unsafe void Swap<T>(T* left, T* right) where T : unmanaged
+    public static class SwapImpl
     {
-        var tmp = *left;
-
-        *left = *right;
-        *right = tmp;
-    }
-
-    public static void Swap<T>(Span<T> span, int left, int right)
-    {
-        var tmp = span[left];
-
-        span[left] = span[right];
-        span[right] = tmp;
-    }
-
-    public static unsafe void SwapIfGreater<T>(T* left, T* right) where T : unmanaged, IComparable<T>
-    {
-        if ((*left).CompareTo(*right) <= 0)
+        public static unsafe void Swap<T>(T* left, T* right) where T : unmanaged
         {
-            return;
+            var tmp = *left;
+
+            *left = *right;
+            *right = tmp;
         }
 
-        Swap(left, right);
+        public static void Swap<T>(Span<T> span, int left, int right)
+        {
+            var tmp = span[left];
+
+            span[left] = span[right];
+            span[right] = tmp;
+        }
+
+        public static unsafe void SwapIfGreater<T>(T* left, T* right) where T : unmanaged, IComparable<T>
+        {
+            if ((*left).CompareTo(*right) <= 0)
+            {
+                return;
+            }
+
+            Swap(left, right);
+        }
     }
 }
 
